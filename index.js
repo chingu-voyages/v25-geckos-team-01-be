8a946ca3e2b9f5963cc2567b4511a401787b71ab
Cesar <cesdia883@gmail.com>
2020-11-10 14:11:52 -0500

    routes folder

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require("express");
+var bodyParser = require("body-parser");
+var dotenv = require("dotenv");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var app = express();
 
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(express.json());
+
+
+var splashRouter = require("./routes/splash")
+app.use("/", splashRouter)
+
+var port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
+    console.log(`Example app listening at http://localhost:${port}`);
+});
