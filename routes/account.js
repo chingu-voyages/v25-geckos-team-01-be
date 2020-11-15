cb4902b39ea79412f15e23960230981dd032e204
Cesar <cesdia883@gmail.com>
2020-11-15 18:28:58 -0500

    generating jwtokens is model method, check existing users on register

--- routes/account.js
+++ routes/account.js
@@ -1,28 +1,50 @@
 const express = require("express");
 const User = require("../models/User");
 const Task = require("../models/Task");
-const jwt = require("jsonwebtoken");
-const checkLoginStatus = require("../services/authServices");
+const {
+    checkLoginStatus,
+    checkExistingUsers,
+} = require("../services/authServices");
 
 const router = express.Router();
 
-//TODO: Seperate the Organization and volunteers function
-
-// TODO: if user is an organization also list out their tasks
-
-// on Postman Headers -- token == Bearer: <token>
-
 // Get the Users Account
 router.get("/", checkLoginStatus, async (req, res) => {
-    try{
-        const user = await User.findById(req.user.id)
-        res.status(200).json(user)
+    try {
+        const user = await User.findById(req.user._id);
+        // logic separating volunteer and organization account pages
+        if (user.role === "organization") {
+            const userTasks = await Task.find({ postedBy: user._id });
+            res.status(200).json({
+                data: user.returnableAuthJson(),
+                tasks: userTasks,
+            });
+        } else {
+            // user is a volunteer
+            res.status(200).json({ user: user.returnableAuthJson() });
+        }
     } catch (err) {
-        res.status(400)
+        res.status(400);
     }
 });
 
 // Update the Users account
-router.put("/", checkLoginStatus, async (req, res) => {});
+
+// Finish this logic
+router.put("/", checkLoginStatus, async (req, res) => {
+    try {
+        let user = await User.findById(req.user._id);
+        user.update({
+            name: req.body.name,
+            email: req.body.email,
+            phoneNumber: req.body.phoneNumber,
+            role: req.body.role,
+            description: req.body.description,
+            tags: req.body.tags,
+        });
+    } catch (error) {
+        res.status(500).json({ Error: error });
+    }
+});
 
 module.exports = router;
