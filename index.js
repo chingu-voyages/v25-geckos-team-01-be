06ca3fbb99f41e2abf198a571a1983e9e3580c7a
Cesar <cesdia883@gmail.com>
2020-11-22 15:49:35 -0500

    cors

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json()); // parse application/json
 // CORS DEV
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "127.0.0.1:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
 });
 
